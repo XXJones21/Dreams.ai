c2ddf46755c0396ea71a52c3f0a7357be5b504e2
Joshua Jones <34466796+XXJones21@users.noreply.github.com>
2025-06-29 20:33:20 -0700

    Premium Cosmic Glass UI Design

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import HeroSection from './components/HeroSection';
-import CosmicBackground from './components/CosmicBackground';
-import AgentShowcase from './components/AgentShowcase';
-import DreamCarousel from './components/DreamCarousel';
-import ProfilePage from './components/profile/ProfilePage';
-import FeedPage from './pages/FeedPage';
-
-function HomePage() {
-  return (
-    <div className="min-h-screen bg-black-marble overflow-hidden">
-      <CosmicBackground />
-      <Header />
-      <HeroSection />
-      <DreamCarousel />
-      <AgentShowcase />
-    </div>
-  );
-}
+import CosmicGlassShowcase from './components/CosmicGlass';
+import './styles/cosmic-glass.css';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/feed" element={<FeedPage />} />
-        <Route path="/dreams" element={<FeedPage />} />
-        {/* Catch all route */}
-        <Route path="*" element={<HomePage />} />
-      </Routes>
-    </Router>
+    <div className="App">
+      <CosmicGlassShowcase />
+    </div>
   );
 }
 
